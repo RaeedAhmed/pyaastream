12aea22315407153aae45fd26a10ecb4ee988d0d
RaeedAhmed <ruahmed7868@gmail.com>
2022-03-28 08:48:26 -0700

    Add cli color/formatting, Add direct uri

--- src/pyaastream/main.py
+++ src/pyaastream/main.py
@@ -10,32 +10,28 @@ from typing import NamedTuple
 from urllib.error import HTTPError, URLError
 
 from bs4 import BeautifulSoup as bs
+from sty import ef, fg, rs
 
 from pyaastream.terms import formats
 
+# type aliases
 url = str
 html = str
 soup = bs
+
+# cli display codes
 LOADING = 0
 SEARCH = 1
 RESULTS = 2
 FILES = 3
+
+# change functions if on win
 DOS = platform.system() == "Windows"
 
 BASE = "https://nyaa.si"
 TEMPFILE = "tmp.torrent"
 TEMPDIR = "webtorrent_tmp"
-
 PLAYER = "mpv" if shutil.which("mpv") else "vlc"
-
-
-class Prompt:
-    query: str
-    torrents: list[str]
-    torrent: int
-    files: list[str]
-    show_all_files: bool = False
-    show_all_torrents: bool = False
 
 
 class Torrent(NamedTuple):
@@ -47,9 +43,48 @@ class Torrent(NamedTuple):
     seeders: int
 
 
+class Prompt:
+    query: str
+    torrents: list[Torrent]
+    torrent: int
+    files: list[str]
+    file_index = -1
+    show_all_files: bool = False
+    show_all_torrents: bool = False
+
+
+# text styling
+def key(command: str):
+    if command.isdigit():
+        k, rest = command, ""
+    else:
+        k, rest = command[0], command[1:]
+    return fg(220) + ef.bold + ef.dim + "[" + k + "]" + rs.all + rest
+
+
+def title(title: str, columns: int, offset=0):
+    if len(title) > columns - offset:
+        title = title[:columns - offset - 3] + "..."
+    return fg(43) + title + rs.all
+
+
+def info(info: list[str], torrent: Torrent):
+    labels = []
+    for attr in info:
+        labels.append(f"{attr}: {getattr(torrent, attr)}")
+    return fg.da_grey + "\t" + ", ".join(labels) + rs.all
+
+
+def header(header: str, columns=1000):
+    if len(header) > columns:
+        header = header[:columns-3] + "..."
+    return fg(161) + header + rs.all
+
+
 def construct_url() -> url:
     Prompt.query = input(f"Search {BASE}: ")
-    filters = {"f": 1, "c": "1_2", "s": "seeders", "o": "desc", "q": Prompt.query}
+    filters = {"f": 1, "c": "1_2", "s": "seeders",
+               "o": "desc", "q": Prompt.query}
     params = urllib.parse.urlencode(filters)
     return f"{BASE}?{params}"
 
@@ -112,33 +147,40 @@ def display(context: int) -> None:
     clear()
     term_size = shutil.get_terminal_size()
     if context == SEARCH:
-        print("Ctrl-C to exit")
+        print(header("Ctrl-C to exit"))
     if context == LOADING:
         pad = "\n" * (round(term_size.lines / 2))
         print(f"{pad}{'Loading...'.center(term_size.columns)}{pad[:-1]}")
     if context == RESULTS:
-        print(f"Search results for '{Prompt.query}':")
+        print(header(f"Search results for '{Prompt.query}':"))
         torrents = (
             Prompt.torrents
             if Prompt.show_all_torrents
             else Prompt.torrents[: (math.floor(term_size.lines / 2) - 2)]
         )
         for index, torrent in enumerate(torrents):
-            print(f"{index:2}:  {torrent.title[:(term_size.columns-5)]}")
             print(
-                f"\t\tsize: {torrent.size}, date: {torrent.date}, seeders: {torrent.seeders}"
-            )
+                f"{key(str(index)):28}{title(torrent.title, term_size.columns, offset=6)}")
+            print(info(["size", "date", "seeders"], torrent))
     if context == FILES:
+        torrent_title = Prompt.torrents[int(Prompt.torrent)].title
+        print(header(torrent_title, term_size.columns))
+        print(header(f"Page: {Prompt.torrents[int(Prompt.torrent)].link}"))
         files = (
             Prompt.files
             if Prompt.show_all_files
             else [file for file in Prompt.files if any(fmt in file for fmt in formats)]
         )
-        print(*files, sep="\n")
-        print(f"Page: {Prompt.torrents[int(Prompt.torrent)].link}")
+        for file in files:
+            index, file_name = file.split(
+                " ")[0], " ".join(file.split(" ")[1:])
+            print(key(index), title(file_name, term_size.columns, offset=6))
 
 
 def cli() -> None:
+    if DOS:
+        # used to init escape codes on windows cmd
+        subprocess.run("", shell=True)
     while True:
         display(SEARCH)
         try:
@@ -147,35 +189,59 @@ def cli() -> None:
             continue
         while True:
             display(RESULTS)
-            t_choice = input("[b]ack, [t]oggle show all, or Choose torrent: ")
-            if t_choice.isdigit() and int(t_choice) in range(len(Prompt.torrents)):
-                Prompt.torrent = int(t_choice)
-                Prompt.files = fetch_files(Prompt.torrents[Prompt.torrent].manifest)
-            elif t_choice == "b":
+            torrent_index = input(
+                f"{key('back')}, {key('show all')}, or Choose torrent: ")
+            if torrent_index.isdigit() and int(torrent_index) in range(len(Prompt.torrents)):
+                Prompt.torrent = int(torrent_index)
+                Prompt.files = fetch_files(
+                    Prompt.torrents[Prompt.torrent].manifest)
+            elif torrent_index == "b":
                 break
-            elif t_choice == "t":
+            elif torrent_index == "s":
                 Prompt.show_all_torrents = not Prompt.show_all_torrents
                 continue
             else:
                 continue
             while True:
                 display(FILES)
-                f_choice = input("[b]ack, [t]oggle show all, or Choose file: ")
-                if f_choice.isdigit() and int(f_choice) in range(len(Prompt.files)):
-                    try:
-                        subprocess.run(
-                            shlex.split(
-                                f'webtorrent{".cmd"*DOS} download {TEMPFILE} -o {TEMPDIR} -s {f_choice} --{PLAYER}'
-                            )
-                        )
-                    except KeyboardInterrupt:
-                        print("Stopping stream")
-                elif f_choice == "b":
+                file_index = input(
+                    f"{key('back')}, {key('show all')}, or Choose file: ")
+                if file_index.isdigit() and int(file_index) in range(len(Prompt.files)):
+                    stream_file(file_index)
+                elif file_index == "b":
                     break
-                elif f_choice == "t":
+                elif file_index == "s":
                     Prompt.show_all_files = not Prompt.show_all_files
                 else:
                     continue
+
+
+def stream(target, choice=""):
+    try:
+        subprocess.run(
+            shlex.split(
+                f'webtorrent{".cmd"*DOS} {target} -o {TEMPDIR} --{PLAYER}'
+            )
+        )
+    except KeyboardInterrupt:
+        print("Stopping stream")
+
+
+def stream_file(file_index):
+    return stream(target=TEMPFILE, choice=f"-s {file_index}")
+
+
+def stream_uri(uri):
+    return stream(target=f'"{uri}"')
+
+
+def direct():
+    try:
+        stream_uri(input("Enter uri: "))
+    except KeyboardInterrupt:
+        exit()
+    finally:
+        shutil.rmtree(TEMPDIR, ignore_errors=True)
 
 
 def main():
